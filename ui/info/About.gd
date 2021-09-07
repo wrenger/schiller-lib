5b6898c0e235eb472f46a5f1f48003f792e120d4
Lars Wrenger <lars@wrenger.net>
2021-09-07 18:56:08 +0200

    :page_facing_up: Update license info

--- ui/info/About.gd
+++ ui/info/About.gd
@@ -7,10 +7,15 @@ func _ready() -> void:
     $Name.text = Util.trf(".info.about", [about.name, about.version])
     $Description.text = about.description
     $Repo/Url.text = about.repository
-    $License.text = Util.trf(".info.about.license", [about.license])
+    $Box/License.text = Util.trf(".info.about.license", [about.license])
     $Devs.text = about.authors.join("\n")
 
 
 func _on_repo_pressed() -> void:
     var result := OS.shell_open($Repo/Url.text)
+    assert(result == OK)
+
+
+func _on_license_godot_pressed() -> void:
+    var result := OS.shell_open($Box/Godot.hint_tooltip)
     assert(result == OK)
