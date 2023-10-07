22995c11fcc20da984ee1d40ca81a77a28b1abee
nwrenger <nils@wrenger.net>
2023-10-07 12:31:21 +0200

    :wrench: Fixed Up Properties

--- lib-view/src/routes/books/BookList.svelte
+++ lib-view/src/routes/books/BookList.svelte
@@ -20,52 +20,53 @@
 	let entry: Book;
 </script>
 
-<div class="list">
-	<div class="card">
-		<div class="card-header d-flex justify-content-between">
-			Title / Authors
-			<span>Id / Status</span>
-		</div>
-		<ul class="list-group list-group-flush list-body">
-			{#each items as item}
-				<button
-					class="list-group-item list-group-item-action d-flex justify-content-between"
-					class:active={item === entry}
-					on:click={() => {entry = item; console.log("Show:", entry)}}
-				>
-					<div class="d-flex flex-column">
-						<p class="mb-0">{item.title}</p>
-						<small class="text-muted">{item.authors.join(", ")}</small>
-					</div>
-					<div class="d-flex flex-column align-items-end">
-						<small class="text-muted">{item.id}</small>
-						<p class="mb-0">{item.borrowed ? "Borrowed" : "Available"}</p>
-					</div>
-				</button>
-			{/each}
-		</ul>
-		<div class="card-footer d-flex justify-content-between align-items-center">
-			{items.length} Results
+<div class="card list">
+	<div class="card-header d-flex justify-content-between">
+		Title / Authors
+		<span>Id / Status</span>
+	</div>
+	<ul class="list-group list-group-flush list-body">
+		{#each items as item}
 			<button
-				class="btn btn-primary"
-				type="button"
-				title="Add"
-				on:click={() => console.log("Initiate Add")}
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="16"
-					height="16"
-					fill="currentColor"
-					class="bi bi-plus-lg"
-					viewBox="0 0 16 16"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-					/>
-				</svg></button
+				class="list-group-item list-group-item-action d-flex justify-content-between"
+				class:active={item === entry}
+				on:click={() => {
+					entry = item;
+					console.log("Show:", entry);
+				}}
 			>
-		</div>
+				<div class="d-flex flex-column">
+					<p class="mb-0">{item.title}</p>
+					<small class="text-muted">{item.authors.join(", ")}</small>
+				</div>
+				<div class="d-flex flex-column align-items-end">
+					<small class="text-muted">{item.id}</small>
+					<p class="mb-0">{item.borrowed ? "Borrowed" : "Available"}</p>
+				</div>
+			</button>
+		{/each}
+	</ul>
+	<div class="card-footer d-flex justify-content-between align-items-center">
+		{items.length} Results
+		<button
+			class="btn btn-primary"
+			type="button"
+			title="Add"
+			on:click={() => console.log("Initiate Add")}
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				class="bi bi-plus-lg"
+				viewBox="0 0 16 16"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+				/>
+			</svg></button
+		>
 	</div>
 </div>
 
@@ -77,7 +78,6 @@
 		width: 50%;
 	}
 	.list-body {
-		height: fit-content;
 		overflow-y: scroll;
 	}
 	@media (max-width: 768px) {
--- lib-view/src/routes/users/UserList.svelte
+++ lib-view/src/routes/users/UserList.svelte
@@ -14,47 +14,52 @@
 	let entry: User;
 </script>
 
-<div class="list">
-	<div class="card">
-		<div class="card-header d-flex justify-content-between align-items-center">
-			Name / Account
-			<span>Role</span>
-		</div>
-		<ul class="list-group list-group-flush list-body">
-			{#each items as item}
-				<button
-					class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
-					class:active={item === entry}
-					on:click={() => {entry = item; console.log("Show:", entry)}}
-				>
-					<div class="d-flex flex-column">
-						<p class="mb-0">{item.forename} {item.surname}</p>
-						<small class="text-muted">{item.account}</small>
-					</div>
-					<div class="d-flex flex-column align-items-end">
-						<p class="mb-0">{item.role}</p>
-					</div>
-				</button>
-			{/each}
-		</ul>
-		<div class="card-footer d-flex justify-content-between align-items-center">
-			{items.length} Results
-			<button class="btn btn-primary" type="button" title="Add" on:click={() => console.log("Initiate Add")}
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="16"
-					height="16"
-					fill="currentColor"
-					class="bi bi-plus-lg"
-					viewBox="0 0 16 16"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-					/>
-				</svg></button
+<div class="card list">
+	<div class="card-header d-flex justify-content-between align-items-center">
+		Name / Account
+		<span>Role</span>
+	</div>
+	<ul class="list-group list-group-flush list-body">
+		{#each items as item}
+			<button
+				class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+				class:active={item === entry}
+				on:click={() => {
+					entry = item;
+					console.log("Show:", entry);
+				}}
 			>
-		</div>
+				<div class="d-flex flex-column">
+					<p class="mb-0">{item.forename} {item.surname}</p>
+					<small class="text-muted">{item.account}</small>
+				</div>
+				<div class="d-flex flex-column align-items-end">
+					<p class="mb-0">{item.role}</p>
+				</div>
+			</button>
+		{/each}
+	</ul>
+	<div class="card-footer d-flex justify-content-between align-items-center">
+		{items.length} Results
+		<button
+			class="btn btn-primary"
+			type="button"
+			title="Add"
+			on:click={() => console.log("Initiate Add")}
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				class="bi bi-plus-lg"
+				viewBox="0 0 16 16"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+				/>
+			</svg></button
+		>
 	</div>
 </div>
 
