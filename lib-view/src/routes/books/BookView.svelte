859d560285bcc14410e93c8f8c4f04a17903129c
nwrenger <nils@wrenger.net>
2023-10-11 22:06:26 +0200

    :sparkles: Now Using Bootstrap Icons

--- lib-view/src/routes/books/BookView.svelte
+++ lib-view/src/routes/books/BookView.svelte
@@ -138,22 +138,7 @@
 				isNew = false;
 			}}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentCol ps-0or"
-				class="bi bi-pencil-square"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-				/>
-				<path
-					fill-rule="evenodd"
-					d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-				/>
-			</svg>
+			<i class="bi bi-pencil-square" />
 		</button>
 		<button
 			id="cancel"
@@ -167,18 +152,7 @@
 				editable = false;
 			}}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentCol ps-0or"
-				class="bi bi-x-lg"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
-				/>
-			</svg>
+			<i class="bi bi-x-lg" />
 		</button>
 	</div>
 
@@ -201,22 +175,7 @@
 					disabled={!editable}
 					on:click={() => console.log("Generate Id")}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						class="bi bi-arrow-repeat"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"
-						/>
-						<path
-							fill-rule="evenodd"
-							d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
-						/>
-					</svg>
+					<i class="bi bi-arrow-repeat" />
 				</button>
 			</div>
 		</div>
@@ -238,21 +197,7 @@
 					disabled={!editable}
 					on:click={() => console.log("Autofill")}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						class="bi bi-upload"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"
-						/>
-						<path
-							d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"
-						/>
-					</svg>
+					<i class="bi bi-upload" />
 				</button>
 			</div>
 		</div>
--- lib-view/src/routes/users/UserView.svelte
+++ lib-view/src/routes/users/UserView.svelte
@@ -88,22 +88,7 @@
 				isNew = false;
 			}}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentCol ps-0or"
-				class="bi bi-pencil-square"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-				/>
-				<path
-					fill-rule="evenodd"
-					d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-				/>
-			</svg>
+			<i class="bi bi-pencil-square" />
 		</button>
 		<button
 			id="cancel"
@@ -117,18 +102,7 @@
 				editable = false;
 			}}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentCol ps-0or"
-				class="bi bi-x-lg"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
-				/>
-			</svg>
+			<i class="bi bi-x-lg" />
 		</button>
 	</div>
 
@@ -177,21 +151,7 @@
 					disabled={!editable}
 					on:click={() => console.log("Autofill")}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						class="bi bi-upload"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"
-						/>
-						<path
-							d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"
-						/>
-					</svg>
+					<i class="bi bi-upload" />
 				</button>
 			</div>
 		</div>
