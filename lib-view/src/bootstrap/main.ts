383d40a96a34d2c729e5985af98b84d08b0afc3c
nwrenger <nils@wrenger.net>
2023-11-26 14:46:55 +0100

    :sparkles: Removed Offcanvas and added icons

--- lib-view/src/bootstrap/main.ts
+++ lib-view/src/bootstrap/main.ts
@@ -1,3 +1,2 @@
-import "bootstrap/js/dist/offcanvas";
 import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/dropdown";
--- lib-view/src/components/basic/Navbar.svelte
+++ lib-view/src/components/basic/Navbar.svelte
@@ -4,44 +4,33 @@
 	import Settings from "../settings/Settings.svelte";
 </script>
 
-<nav class="navbar navbar-expand-sm bg-body-tertiary">
+<nav class="navbar navbar-expand bg-body-tertiary">
 	<div class="container-fluid">
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="offcanvas"
-			data-bs-target="#offcanvasNavbar"
-			aria-controls="offcanvasNavbar"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon" />
-		</button>
-		<div
-			class="offcanvas offcanvas-end"
-			tabindex="-1"
-			id="offcanvasNavbar"
-			aria-labelledby="offcanvasNavbarLabel"
-		>
-			<div class="offcanvas-header">
-				<h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
-			</div>
-			<div class="offcanvas-body">
-				<div class="navbar-nav flex-grow-1 pe-3">
-					<a class={"nav-link"} class:active={$page.url.pathname === "/books"} href="/books">
-						{$_(".search.book")}
-					</a>
-					<a class={"nav-link"} class:active={$page.url.pathname === "/users"} href="/users">
-						{$_(".search.user")}
-					</a>
-					<a class={"nav-link"} class:active={$page.url.pathname === "/overdues"} href="/overdues">
-						{$_(".book.overdues")}
-					</a>
-					<a class={"nav-link"} class:active={$page.url.pathname === "/info"} href="/info">
-						{$_(".alert.info")}
-					</a>
+		<div class="navbar-nav">
+			<a class={"nav-link"} class:active={$page.url.pathname === "/books"} href="/books">
+				<div class="m-0 fs-6 text-truncate text-mobile">
+					<i class="bi bi-book" title={$_(".search.book")} />
+					{$_(".search.book")}
 				</div>
-			</div>
+			</a>
+			<a class={"nav-link"} class:active={$page.url.pathname === "/users"} href="/users">
+				<div class="m-0 fs-6 text-truncate text-mobile">
+					<i class="bi bi-people" title={$_(".search.user")} />
+					{$_(".search.user")}
+				</div>
+			</a>
+			<a class={"nav-link"} class:active={$page.url.pathname === "/overdues"} href="/overdues">
+				<div class="m-0 fs-6 text-truncate text-mobile">
+					<i class="bi bi-clock" title={$_(".book.overdues")} />
+					{$_(".book.overdues")}
+				</div>
+			</a>
+			<a class={"nav-link"} class:active={$page.url.pathname === "/info"} href="/info">
+				<div class="m-0 fs-6 text-truncate text-mobile">
+					<i class="bi bi-info-circle" title={$_(".alert.info")} />
+					{$_(".alert.info")}
+				</div>
+			</a>
 		</div>
 		<Settings />
 	</div>
