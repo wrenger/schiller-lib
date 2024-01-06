1b59148719318923a2c28abbd54ad66ba110e136
Lars Wrenger <lars@wrenger.net>
2024-01-06 11:54:34 +0100

    :art: Minor changes

--- test/data/cert/gen.sh
+++ test/data/cert/gen.sh
@@ -2,4 +2,4 @@
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
   -nodes -keyout test/data/cert/key.pem -out test/data/cert/cert.pem -subj "/CN=127.0.0.1"
-#   -addext "subjectAltName=DNS:lib.wrenger.net,DNS:*.lib.wrenger.net,IP:127.0.0.1"
+#   -addext "subjectAltName=DNS:example.com,DNS:*.example.com,IP:127.0.0.1"
